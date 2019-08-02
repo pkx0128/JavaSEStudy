c14f50fb1884879a6ccbf9ac6bc9aef0b4ccf88b
pankx <764670547@qq.com>
2019-08-03 00:21:01 +0800

    Arrays.sort()的用法

--- src/JavaSEStudy.java
+++ src/JavaSEStudy.java
@@ -1,9 +1,14 @@
-
+import java.util.Arrays;
 public class JavaSEStudy {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int[] num = {3,4,2,6,9,1};
+		Arrays.sort(num);
+		for(int i:num) {
+			
+			System.out.println(i);
+		}
 	}
 
 }
