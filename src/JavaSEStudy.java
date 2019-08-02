fc206afc93a319e199e0f72f023bca731546f9c3
pankx <764670547@qq.com>
2019-08-03 00:25:40 +0800

    添加注释

--- src/JavaSEStudy.java
+++ src/JavaSEStudy.java
@@ -3,10 +3,12 @@ public class JavaSEStudy {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//定义整型数组 num
 		int[] num = {3,4,2,6,9,1};
+		//调用Arrays.sort()方法对数组num元素进行排序
 		Arrays.sort(num);
-		for(int i:num) {
-			
+		//遍历数组元素
+		for(int i:num) {	
 			System.out.println(i);
 		}
 	}
