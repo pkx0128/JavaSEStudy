6d14c15cc7a33c82a6c2da1f05c2ea0882a15723
pankx <764670547@qq.com>
2019-08-08 02:42:34 +0800

    引入用了ClassDemo包中的Student类后，直接用类名可方法其他包中的类

--- src/packageDemo/packageDemo.java
+++ src/packageDemo/packageDemo.java
@@ -1,11 +1,15 @@
 package packageDemo;
-
+//引入ClassDemo包中的Student类
+import ClassDemo.Student;
 public class packageDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 //		未引用包时，跨包访问来用包名
-		ClassDemo.Student std = new ClassDemo.Student("pankx");
+//		ClassDemo.Student std = new ClassDemo.Student("pankx");
+//		引入用了ClassDemo包中的Student类后，直接用类名可方法其他包中的类
+		Student std = new Student("pankx");
+		
 	}
 
 }
