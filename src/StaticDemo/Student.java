bdb1a5a9d75c6cdec66ab5425f5953208a502398
pankx <764670547@qq.com>
2019-08-08 16:56:15 +0800

    定义类的静态代码块，主要用于初始化静态属性的值

--- src/StaticDemo/Student.java
+++ src/StaticDemo/Student.java
@@ -5,6 +5,11 @@ public class Student {
 	public static String schoolName;
 	public String name;
 	public String sex;
+//	定义类的静态代码块，主要用于初始化静态属性的值
+	static {
+		schoolName = "市二中";
+		System.out.println("类的静态代码块！");
+	}
 //	定义静态方法
 	public static void sayHi() {
 		System.out.println("This is static method");
