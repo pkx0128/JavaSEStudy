eca3e5e98278bf1ecbe1ddcd6fa662806c408264
pankx <764670547@qq.com>
2019-08-10 12:54:39 +0800

    有提示信息的断言实例

--- src/ExceptionDemo/AssertionDemo.java
+++ src/ExceptionDemo/AssertionDemo.java
@@ -5,9 +5,9 @@ public class AssertionDemo {
 	private int b;
 	private int c;
 	public AssertionDemo(int a,int b,int c) {
-		assert(a+b<c);
-		assert(a+c<b);
-		assert(b+c<a);
+		assert(a+b<c):"不满足三角形三边条件";
+		assert(a+c<b):"不满足三角形三边条件";
+		assert(b+c<a):"不满足三角形三边条件";
 		
 		this.a = a;
 		this.b = b;
