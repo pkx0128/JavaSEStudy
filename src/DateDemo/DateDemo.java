c4a6dedb4e6f10625440834cdb33c7d4c11244e2
pankx <764670547@qq.com>
2019-08-13 02:30:06 +0800

    使用Calendar类实现计算指定生日距离当前时间还有几天的功能

--- src/DateDemo/DateDemo.java
+++ src/DateDemo/DateDemo.java
@@ -1,5 +1,6 @@
 package DateDemo;
 
+import java.util.Calendar;
 import java.util.Date;
 public class DateDemo {
 
@@ -8,6 +9,28 @@ public class DateDemo {
 		Date date = new Date();
 		System.out.println(date.toString());
 		
+//		用Calendar类计算当前日期距离生日多少天
+//		生日1月28号
+		Calendar cnow = Calendar.getInstance();
+		Calendar cbirthday = Calendar.getInstance();
+		if(cnow.get(Calendar.MONTH)>0) {
+			cbirthday.set(Calendar.YEAR,cnow.get(Calendar.YEAR)+1);
+		}else if(cnow.get(Calendar.MONTH) == 0 && cnow.get(Calendar.DAY_OF_MONTH)>28) {
+			cbirthday.set(Calendar.YEAR,cnow.get(Calendar.YEAR)+1);
+		}
+		cbirthday.set(Calendar.MONTH,0);
+		cbirthday.set(Calendar.DAY_OF_MONTH,28);
+		
+		long cbirthdayMillis = cbirthday.getTimeInMillis();
+		long cnowMillis = cnow.getTimeInMillis();
+		long dayMillis = 0;
+		if(cnow.after(cbirthday)) {
+			dayMillis = cnowMillis - cbirthdayMillis;
+		}else {
+			dayMillis = cbirthdayMillis - cnowMillis;
+		}
+			int day = (int)(dayMillis/1000/60/60/24);	
+			System.out.println("距离你的生日还有"+day+"天");
 	}
 	
 	
