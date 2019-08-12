093bfd1bc4411f66d025179d4f7ff2d1aacdc371
pankx <764670547@qq.com>
2019-08-13 01:45:45 +0800

    Date类的简单实例

---
+++ src/DateDemo/DateDemo.java
@@ -0,0 +1,15 @@
+package DateDemo;
+
+import java.util.Date;
+public class DateDemo {
+
+	public static void main(String[] agrs) {
+//		Date的简单实例
+		Date date = new Date();
+		System.out.println(date.toString());
+		
+	}
+	
+	
+	
+}
