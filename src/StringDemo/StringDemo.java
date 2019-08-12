8e1e113a4ef2b7060a68d8627d20695476fd897a
pankx <764670547@qq.com>
2019-08-13 00:01:44 +0800

    String的split方法实例

---
+++ src/StringDemo/StringDemo.java
@@ -0,0 +1,14 @@
+package StringDemo;
+
+public class StringDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "This is a java project";
+		String[] sArray = str.split(" ");
+		for(String ss:sArray) {
+			System.out.println(ss);
+		}
+	}
+
+}
