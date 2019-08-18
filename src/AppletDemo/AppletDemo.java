90c2a1c8df19f97f66c06232d2506e9b830db4bc
pankx <764670547@qq.com>
2019-08-18 12:19:34 +0800

    Applet实例

---
+++ src/AppletDemo/AppletDemo.java
@@ -0,0 +1,10 @@
+package AppletDemo;
+
+import java.applet.*;
+import java.awt.Graphics;
+
+public class AppletDemo extends Applet {
+	public void paint(Graphics g) {
+		g.drawString("Applet Demo", 100, 10);
+	}
+}
